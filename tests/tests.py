837f24fc3bbf1de238b6211d8982ffd26e31343a
Cenk Alti <cenkalti@gmail.com>
2013-03-09 12:17:28 +0200

    fix tests

--- tests/tests.py
+++ tests/tests.py
@@ -6,14 +6,16 @@ import unittest
 TESTS_ROOT = os.path.abspath(os.path.dirname(__file__))
 sys.path.insert(0, os.path.realpath(os.path.join(TESTS_ROOT, '..')))
 
-from kuyruk import task
+from kuyruk import Kuyruk
 from kuyruk.task import Task
 
 
 class KuyrukTestCase(unittest.TestCase):
 
     def test_task_decorator(self):
-        @task
+        kuyruk = Kuyruk()
+
+        @kuyruk.task
         def print_task(message):
             print message
 
