48a64c5797d53b560e1b8421778bdccbc5a28fe3
Cenk Alti <cenkalti@gmail.com>
2013-03-09 14:54:26 +0200

    pass tests

--- tests/tests.py
+++ tests/tests.py
@@ -2,6 +2,8 @@ import os
 import sys
 import unittest
 import threading
+from time import sleep
+from contextlib import contextmanager
 
 # HACK: Prepend ../ to PYTHONPATH so that we can import muhtar form there.
 TESTS_ROOT = os.path.abspath(os.path.dirname(__file__))
@@ -11,12 +13,21 @@ from kuyruk import Kuyruk
 from kuyruk.task import Task
 
 
+@contextmanager
 def run_kuyruk(kuyruk, queue='kuyruk'):
     thread = threading.Thread(target=kuyruk.run, args=(queue, ))
     thread.start()
+    yield
+    sleep(1)
+    kuyruk.exit = True
+    thread.join()
 
-
+kuyruk = Kuyruk()
+called = False
+@kuyruk.task
 def print_task(message):
+    global called
+    called = True
     print message
 
 
@@ -32,17 +43,10 @@ class KuyrukTestCase(unittest.TestCase):
         self.assertIsInstance(print_task, Task)
 
     def test_simple_task(self):
-        kuyruk = Kuyruk()
+        with run_kuyruk(kuyruk):
+            print_task('hello world')
 
-        _print = kuyruk.task(print_task)
-
-        run_kuyruk(kuyruk)
-
-        _print('hello world')
-
-        import time
-        time.sleep(2)
-        kuyruk.exit = True
+        self.assertEqual(called, True)
 
 
 if __name__ == '__main__':
