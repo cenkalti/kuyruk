9ae35384f1323d22fa45bdd69dccc1446214b16a
Cenk Alti <cenkalti@gmail.com>
2013-03-20 10:05:29 +0200

    tests

--- tests/tests.py
+++ tests/tests.py
@@ -14,14 +14,35 @@ sys.path.insert(0, os.path.realpath(os.path.join(TESTS_ROOT, '..')))
 
 from kuyruk import Kuyruk, Task
 
+kuyruk = Kuyruk()
+called = False
 
-def exit_on_excption(f):
-    def inner(*args, **kwargs):
-        try:
-            return f(*args, **kwargs)
-        except Exception:
-            traceback.print_exc()
-    return inner
+
+@kuyruk.task
+def print_task(message):
+    global called
+    called = True
+    print message
+
+
+@kuyruk.task(queue='another_queue')
+def print_task2(message):
+    global called
+    called = True
+    print message
+
+
+class KuyrukTestCase(unittest.TestCase):
+
+    def test_task_decorator(self):
+        self.assertIsInstance(print_task, Task)
+        self.assertIsInstance(print_task2, Task)
+
+    def test_simple_task(self):
+        with run_kuyruk(kuyruk):
+            print_task('hello world')
+
+        self.assertEqual(called, True)
 
 
 @contextmanager
@@ -34,31 +55,14 @@ def run_kuyruk(kuyruk, queue='kuyruk'):
     kuyruk.exit = True
     thread.join()
 
-kuyruk = Kuyruk()
-called = False
-@kuyruk.task
-def print_task(message):
-    global called
-    called = True
-    print message
 
-
-class KuyrukTestCase(unittest.TestCase):
-
-    def test_task_decorator(self):
-        self.assertIsInstance(print_task, Task)
-
-        @kuyruk.task(queue='another_queue')
-        def _task():
-            pass
-
-        self.assertIsInstance(_task, Task)
-
-    def test_simple_task(self):
-        with run_kuyruk(kuyruk):
-            print_task('hello world')
-
-        self.assertEqual(called, True)
+def exit_on_excption(f):
+    def inner(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except Exception:
+            traceback.print_exc()
+    return inner
 
 
 if __name__ == '__main__':
