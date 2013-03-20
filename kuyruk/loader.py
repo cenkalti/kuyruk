6dad92520cc61c1821cc57312bf6422aa69b1072
Cenk Alti <cenkalti@gmail.com>
2013-03-20 08:32:01 +0200

    reorder functions

--- kuyruk/loader.py
+++ kuyruk/loader.py
@@ -4,6 +4,27 @@ import importlib
 from contextlib import contextmanager
 
 
+def import_task(fully_qualified_function_name):
+    module_name, func_name = split_function_name(fully_qualified_function_name)
+    module = import_task_module(module_name)
+    return getattr(module, func_name)
+
+
+def split_function_name(name):
+    reverse_str = lambda s: s[::-1]
+    func_name, module_name = map(reverse_str, reverse_str(name).split('.', 1))
+    return module_name, func_name
+
+
+def import_task_module(module):
+    return import_from_cwd(module)
+
+
+def import_from_cwd(module):
+    with cwd_in_path():
+        return importlib.import_module(module)
+
+
 @contextmanager
 def cwd_in_path():
     cwd = os.getcwd()
@@ -12,33 +33,9 @@ def cwd_in_path():
     else:
         sys.path.insert(0, cwd)
         try:
-            yield cwd
+            yield
         finally:
             try:
                 sys.path.remove(cwd)
-            except ValueError:  # pragma: no cover
+            except ValueError:
                 pass
-
-
-def import_from_cwd(module):
-    with cwd_in_path():
-        return importlib.import_module(module)
-
-
-def import_task_module(module):
-    return import_from_cwd(module)
-
-
-def import_task(fully_qualified_function_name):
-    module_name, func_name = split_function_name(fully_qualified_function_name)
-    module = import_task_module(module_name)
-    return getattr(module, func_name)
-
-
-def split_function_name(name):
-    func_name, module_name = map(reverse_str, reverse_str(name).split('.', 1))
-    return module_name, func_name
-
-
-def reverse_str(s):
-    return s[::-1]
