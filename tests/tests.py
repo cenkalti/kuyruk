5346b269769a46e2e69c8cf8dd80dc24f3b6cd5e
Cenk Alti <cenkalti@gmail.com>
2013-03-23 16:44:50 +0200

    tests

--- tests/tests.py
+++ tests/tests.py
@@ -10,18 +10,20 @@ sys.path.insert(0, os.path.realpath(os.path.join(TESTS_ROOT, '..')))
 
 from kuyruk import Kuyruk, Task, Queue
 
-kuyruk = Kuyruk()
-kuyruk2 = Kuyruk()
+# This global variable will be checked to determine if the tasks have run.
 called = False
 
 
+# These 2 functions below needs to be module level in order that kuyruk
+# to determine their fully qualified name.
+kuyruk = Kuyruk()
 @kuyruk.task
 def print_task(message):
     global called
     called = True
     print message
 
-
+kuyruk2 = Kuyruk()
 @kuyruk2.task(queue='another_queue')
 def print_task2(message):
     global called
@@ -30,6 +32,7 @@ def print_task2(message):
 
 
 def run_kuyruk(kuyruk, max_tasks=1):
+    """Runs in same thread"""
     kuyruk.max_tasks = max_tasks
     kuyruk.run()
 
@@ -45,15 +48,18 @@ class KuyrukTestCase(unittest.TestCase):
         Queue(kuyruk.queue, kuyruk.connection).delete()
 
     def test_task_decorator(self):
+        # Decorator without args
         self.assertIsInstance(print_task, Task)
+        # Decorator with args
         self.assertIsInstance(print_task2, Task)
 
     def test_simple_task(self):
-        print_task('hello world')
+        print_task('hello world')  # sends task
         run_kuyruk(kuyruk)
         self.assertEqual(called, True)
 
     def test_another_queue(self):
+        # Clear another_queue
         Queue('another_queue', kuyruk2.connection).delete()
 
         print_task2('hello world')
