1f30e8fff2b90fb38c9262fa5b354ab09a90d1a2
Cenk Alti <cenkalti@gmail.com>
2013-03-26 13:19:41 +0200

    require importlib for python 2.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='Kuyruk',
     version='0.1',
     packages=['kuyruk'],
-    install_requires=['pika>=0.9.9'],
+    install_requires=['pika>=0.9.9', 'importlib>=1.0.2'],
     entry_points={
         'console_scripts': [
             'kuyruk = kuyruk.__main__:main'
