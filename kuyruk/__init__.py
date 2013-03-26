bc62826eb198b18cd152b514aa20b57fe303c6dd
Cenk Alti <cenkalti@gmail.com>
2013-03-26 13:24:54 +0200

    NullHandler for 2.6

--- kuyruk/__init__.py
+++ kuyruk/__init__.py
@@ -1,4 +1,11 @@
 import logging
+try:
+    # not available in python 2.6
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
 
 from .kuyruk import Kuyruk
 from .task import Task
