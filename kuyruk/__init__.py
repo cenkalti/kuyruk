57bf7d7bae158dbb87185295ad79ed0ea5695b71
Cenk Alti <cenkalti@gmail.com>
2013-04-09 16:37:06 +0300

    update todo items

--- kuyruk/__init__.py
+++ kuyruk/__init__.py
@@ -16,13 +16,10 @@ from .exceptions import Reject
 # Add NullHandler to prevent logging warnings on startup
 logging.getLogger(__name__).addHandler(NullHandler())
 
-# TODO transactions
-# TODO class tasks
-# TODO retry count for tasks
-# TODO packaging test code
+# TODO class tasks (resolver)
 # TODO set kuyruk log level
 # TODO set application log level
-# TODO requeue failed tasks
+# TODO write a script for requeueing failed tasks
 # TODO coverage
 # TODO test disconnect and reconnect (vaurien)
 # TODO model extension
