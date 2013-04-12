56dc3b9a8bdf939a27c31c553cf4221b9e2b9592
Cenk Alti <cenkalti@gmail.com>
2013-04-12 18:34:12 +0300

    alti

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Kuyruk'
-copyright = u'2013, Cenk Alti'
+copyright = u'2013, Cenk Altı'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -188,7 +188,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index', 'Kuyruk.tex', u'Kuyruk Documentation',
-   u'Cenk Alti', 'manual'),
+   u'Cenk Altı', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -218,7 +218,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'kuyruk', u'Kuyruk Documentation',
-     [u'Cenk Alti'], 1)
+     [u'Cenk Altı'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -232,7 +232,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'Kuyruk', u'Kuyruk Documentation',
-   u'Cenk Alti', 'Kuyruk', 'One line description of project.',
+   u'Cenk Altı', 'Kuyruk', 'One line description of project.',
    'Miscellaneous'),
 ]
 
