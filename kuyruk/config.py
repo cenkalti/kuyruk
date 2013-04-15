85e52dc92694f4530e9155da07137bb407060e56
Cenk Alti <cenkalti@gmail.com>
2013-04-15 03:14:38 +0300

    new config

--- kuyruk/config.py
+++ kuyruk/config.py
@@ -1,25 +1,25 @@
 class Config(object):
     """Kuyruk configuration object"""
 
-    DEFAULTS = {
-        'KUYRUK_RABBIT_HOST': 'localhost',
-        'KUYRUK_RABBIT_PORT': 5672,
-        'KUYRUK_RABBIT_USER': 'guest',
-        'KUYRUK_RABBIT_PASSWORD': 'guest',
-        'KUYRUK_EAGER': False,
-        'KUYRUK_MAX_RUN_TIME': None,
-        'KUYRUK_MAX_TASKS': None,
-        'KUYRUK_MAX_LOAD': None,
-        'KUYRUK_WORKERS': {},
-        'KUYRUK_SAVE_FAILED_TASKS': False,
-    }
+    # Defaults
+    RABBIT_HOST = 'localhost'
+    RABBIT_PORT = 5672
+    RABBIT_USER = 'guest'
+    RABBIT_PASSWORD = 'guest'
+    EAGER = False
+    MAX_LOAD = None
+    SAVE_FAILED_TASKS = False
+    WORKERS = {}
 
     def __init__(self, obj):
         """Populate from obj.
-        If the key is not found in obj, set from DEFAULTS.
+        If the key is not found in obj, default is used.
         "KUYRUK_" prefix is stripped in attributes.
 
         """
-        for k, v in self.DEFAULTS.iteritems():
-            value = getattr(obj, k, v)
-            setattr(self, k[7:], value)
+        if not isinstance(obj, dict):
+            obj = obj.__dict__
+
+        for k, v in obj.iteritems():
+            if k.startswith('KUYRUK_'):
+                setattr(self, k[7:], v)
