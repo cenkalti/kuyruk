0d69043ea26b88ff141e6517ffec05979506dbbd
Cenk Alti <cenkalti@gmail.com>
2013-04-24 07:46:05 +0300

    bump version
    [ci skip]

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except ImportError:
 
 setup(
     name='Kuyruk',
-    version='0.3.1',
+    version='0.3.2',
     author=u'Cenk Altı',
     license='Apache License 2.0',
     keywords='rabbitmq distributed task queue',
