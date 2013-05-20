6aa12e976425c5318073013fbc1e7c98934009ac
Cenk Alti <cenkalti@gmail.com>
2013-05-20 22:50:09 +0300

    bump version

--- kuyruk/__init__.py
+++ kuyruk/__init__.py
@@ -6,7 +6,7 @@ from kuyruk.worker import Worker
 from kuyruk.task import Task
 from kuyruk.config import Config
 
-__version__ = '0.12.2'
+__version__ = '0.13.0'
 
 try:
     # not available in python 2.6
