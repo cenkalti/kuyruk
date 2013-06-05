9144bef87a28c0bda42924324f0bfe0a860526ef
Cenk Alti <cenkalti@gmail.com>
2013-06-05 13:56:54 +0300

    bump version

--- kuyruk/__init__.py
+++ kuyruk/__init__.py
@@ -1,4 +1,6 @@
 from __future__ import absolute_import
+import errno
+import select
 import logging
 
 from kuyruk.kuyruk import Kuyruk
@@ -6,7 +8,7 @@ from kuyruk.worker import Worker
 from kuyruk.task import Task
 from kuyruk.config import Config
 
-__version__ = '0.14.7'
+__version__ = '0.15.0'
 
 try:
     # not available in python 2.6
@@ -20,3 +22,17 @@ except ImportError:
 null_handler = NullHandler()
 logging.getLogger('kuyruk').addHandler(null_handler)
 logging.getLogger('pika').addHandler(null_handler)
+
+
+# Monkey patch ReadPoller until pika 0.9.14 is released
+def ready(self):
+    while True:
+        try:
+            return original_method(self)
+            break
+        except select.error as e:
+            if e[0] != errno.EINTR:
+                raise
+from pika.adapters.blocking_connection import ReadPoller
+original_method = ReadPoller.ready
+ReadPoller.ready = ready
