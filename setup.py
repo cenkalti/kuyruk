3aeea411659fe72a1d3f4d235021f20588e050c6
Cenk Alti <cenkalti@gmail.com>
2013-08-02 16:37:26 +0300

    change license to MIT

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
