1ba66c90ff11786f1c701a66068cfe72791d1c11
Cenk Alti <cenkalti@gmail.com>
2013-08-10 12:04:21 +0300

    add test script for testing stuck processes

---
+++ kuyruk/test/forever.py
@@ -0,0 +1,38 @@
+import os
+import sys
+import signal
+import subprocess
+from time import sleep
+
+from kuyruk import Kuyruk
+
+
+kuyruk = Kuyruk()
+
+
+@kuyruk.task(queue='forever')
+def run_forever():
+    path = os.path.abspath(__file__)
+    p = subprocess.Popen([sys.executable, path])
+    p.wait()
+    print "Done."
+
+
+if __name__ == '__main__':
+
+    def handle_signal(signum, frame):
+        print 'SIGNAL', signum
+
+    signal.signal(signal.SIGINT, handle_signal)
+    signal.signal(signal.SIGTERM, handle_signal)
+    signal.signal(signal.SIGABRT, handle_signal)
+    signal.signal(signal.SIGALRM, handle_signal)
+    signal.signal(signal.SIGQUIT, handle_signal)
+    signal.signal(signal.SIGCHLD, handle_signal)
+    signal.signal(signal.SIGUSR1, handle_signal)
+
+    i = 0
+    while True:
+        i += 1
+        print i
+        sleep(1)
