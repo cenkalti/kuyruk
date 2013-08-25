2c6dde6140400ea0bb040b0f87f87c7cf4a8c902
Cenk Alti <cenkalti@gmail.com>
2013-08-25 15:40:57 +0300

    remove nosetests invoke task because when ctrl-c it hides the traceback of tests

--- tasks.py
+++ tasks.py
@@ -2,11 +2,6 @@ from invoke import run, task
 
 
 @task
-def test():
-    run("nosetests")
-
-
-@task
 def bump(part):
     assert part in ('major', 'minor', 'patch')
     run("bumpversion %s" % part)
@@ -17,8 +12,9 @@ def upload():
     run("python setup.py sdist upload")
 
 
-@task('test')
+@task
 def coverage():
+    run("nosetests")
     run("coverage combine")
     run("coverage html")
     run("open coverage_html_report/index.html")
