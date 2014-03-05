b89f3fabe6439dec7f06f2e49c57dc0439b58ab4
Refik Turkeli <refik.rfk@gmail.com>
2014-03-05 12:41:27 +0200

    Bump version: 0.24.2 → 0.24.3

--- kuyruk/__init__.py
+++ kuyruk/__init__.py
@@ -14,7 +14,7 @@ from kuyruk.worker import Worker
 from kuyruk.events import EventMixin
 from kuyruk.connection import Connection
 
-__version__ = '0.24.2'
+__version__ = '0.24.3'
 __all__ = ['Kuyruk', 'Task', 'Worker']
 
 
