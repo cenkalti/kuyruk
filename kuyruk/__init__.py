8ce3e5c8cd63d6b8f9184cd10af0ba3899da308b
Cenk Alti <cenkalti@gmail.com>
2014-11-17 01:05:26 +0200

    Bump version: 1.2.4 → 2.0.0

--- kuyruk/__init__.py
+++ kuyruk/__init__.py
@@ -12,7 +12,7 @@ from kuyruk.config import Config
 from kuyruk.worker import Worker
 from kuyruk.events import EventMixin
 
-__version__ = '1.2.4'
+__version__ = '2.0.0'
 __all__ = ['Kuyruk', 'Config', 'Task', 'Worker']
 
 
