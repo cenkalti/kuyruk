3677f204644291c11b59a861cae397ee78e4c321
Cenk Alti <cenkalti@gmail.com>
2014-12-03 20:15:53 +0200

    add python 3.4 classifier

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Software Development :: Object Brokering',
         'Topic :: System :: Distributed Computing',
     ],
