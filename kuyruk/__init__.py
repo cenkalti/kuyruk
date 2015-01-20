9f08891690cbda540c0be550fee667f28fa6cf68
Cenk Alti <cenkalti@gmail.com>
2015-01-20 15:46:44 +0200

    Bump version: 2.0.1 → 2.0.2

--- kuyruk/__init__.py
+++ kuyruk/__init__.py
@@ -11,7 +11,7 @@ from kuyruk.task import Task
 from kuyruk.config import Config
 from kuyruk.worker import Worker
 
-__version__ = '2.0.1'
+__version__ = '2.0.2'
 __all__ = ['Kuyruk', 'Config', 'Task']
 
 
