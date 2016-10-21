6614f707dfd2f82e4746d16b526ca2488e57ea6a
Vlad Frolov <frolvlad@gmail.com>
2016-10-21 17:04:01 +0300

    Make use of `pkg_resources` optional
    `pkg_resources` is a part of `setuptools` package, which is neither available nor used in Anaconda Python for entrypoints.

--- kuyruk/__main__.py
+++ kuyruk/__main__.py
@@ -7,7 +7,11 @@ from __future__ import absolute_import
 import sys
 import logging
 import argparse
-import pkg_resources
+
+try:
+    import pkg_resources
+except ImportError:
+    pkg_resources = None
 
 from kuyruk import __version__, importer, Kuyruk
 from kuyruk.worker import Worker
@@ -38,13 +42,14 @@ def main():
         '-l', '--local', action="store_true",
         help='append hostname to the queue name')
 
-    # Add additional subcommands from extensions.
-    for entry_point in pkg_resources.iter_entry_points("kuyruk.commands"):
-        command_func, help_text, customize_parser = entry_point.load()
-        ext_parser = subparsers.add_parser(entry_point.name, help=help_text)
-        ext_parser.set_defaults(func=command_func)
-        if customize_parser:
-            customize_parser(ext_parser)
+    if pkg_resources is not None:
+        # Add additional subcommands from extensions.
+        for entry_point in pkg_resources.iter_entry_points("kuyruk.commands"):
+            command_func, help_text, customize_parser = entry_point.load()
+            ext_parser = subparsers.add_parser(entry_point.name, help=help_text)
+            ext_parser.set_defaults(func=command_func)
+            if customize_parser:
+                customize_parser(ext_parser)
 
     # Parse arguments
     args = parser.parse_args()
