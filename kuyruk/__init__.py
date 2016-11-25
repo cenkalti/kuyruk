1e75ab8abe9ad091f54db83cf199ac405763db72
Cenk Alti <cenkalti@gmail.com>
2016-11-25 17:35:11 +0300

    Bump version: 8.0.0 → 8.1.0

--- kuyruk/__init__.py
+++ kuyruk/__init__.py
@@ -9,7 +9,7 @@ from kuyruk.task import Task
 from kuyruk.config import Config
 from kuyruk.worker import Worker
 
-__version__ = '8.0.0'
+__version__ = '8.1.0'
 __all__ = ['Kuyruk', 'Config', 'Task', 'Worker']
 
 
