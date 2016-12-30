432a1ec55b60b8c04006d1bf26b41d5b51d83dbb
Cenk Alti <cenkalti@gmail.com>
2016-12-30 15:20:08 +0300

    compatible with 3.5 and 3.6

--- setup.py
+++ setup.py
@@ -48,6 +48,8 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Object Brokering',
         'Topic :: System :: Distributed Computing',
     ],
