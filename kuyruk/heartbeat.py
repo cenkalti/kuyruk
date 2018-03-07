f3f252c05fc1dd964736294a9d24c557e09987de
Cenk Alti <cenkalti@gmail.com>
2018-03-07 18:07:57 +0300

    more specific type

--- kuyruk/heartbeat.py
+++ kuyruk/heartbeat.py
@@ -2,16 +2,18 @@ import sys
 import socket
 import logging
 import threading
-from typing import Callable, Tuple, Any
+from typing import Callable
 
 import amqp
+
+from kuyruk.exceptions import ExcInfoType
 
 logger = logging.getLogger(__name__)
 
 
 class Heartbeat:
 
-    def __init__(self, connection: amqp.Connection, on_error: Callable[[Tuple[Any, Any, Any]], None]) -> None:
+    def __init__(self, connection: amqp.Connection, on_error: Callable[[ExcInfoType], None]) -> None:
         self._connection = connection
         self._on_error = on_error
         self._stop = threading.Event()
