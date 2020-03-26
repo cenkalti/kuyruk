7209ae46a9c84b9fc518fb2670bdbf7e80c7699e
Cenk Alti <cenkalti@gmail.com>
2020-03-26 14:24:08 +0300

    set TCP_USER_TIMEOUT only on linux

--- kuyruk/kuyruk.py
+++ kuyruk/kuyruk.py
@@ -64,11 +64,10 @@ class Kuyruk:
     @contextmanager
     def connection(self) -> Iterator[amqp.Connection]:
         """Returns a new connection as a context manager."""
-        TCP_USER_TIMEOUT = 18  # constant is available on Python 3.6+.
-        socket_settings = {TCP_USER_TIMEOUT: self.config.TCP_USER_TIMEOUT}
-
-        if sys.platform.startswith('darwin'):
-            del socket_settings[TCP_USER_TIMEOUT]
+        socket_settings = {}
+        if sys.platform.startswith('linux'):
+            TCP_USER_TIMEOUT = 18  # constant is available on Python 3.6+.
+            socket_settings[TCP_USER_TIMEOUT] = self.config.TCP_USER_TIMEOUT
 
         conn = amqp.Connection(
             host="%s:%s" % (self.config.RABBIT_HOST, self.config.RABBIT_PORT),
