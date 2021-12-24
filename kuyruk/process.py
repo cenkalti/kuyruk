18f84fc9910a90284ee2f42db3c3df41347b43ee
BatuAksoy <aksoyb15@itu.edu.tr>
2021-12-24 18:13:01 +0300

    use super

--- kuyruk/process.py
+++ kuyruk/process.py
@@ -1,21 +1,36 @@
 import multiprocessing
+import signal
+import sys
 
 
 class Process(multiprocessing.Process):
     def __init__(self, *args, **kwargs):
-        super().__init__(self, *args, **kwargs)
+        super().__init__(*args, **kwargs)
 
         self._queue = multiprocessing.Queue(maxsize=1)
         self._result = None
 
     def run(self):
         try:
+            self._clear_signal_handlers()
             result = self._target(*self._args, **self._kwargs)
             self._queue.put((result, None))
         except Exception as exc:
             self._queue.put((None, exc))
+        finally:
+            self._queue.close()
+
+    def _handle_signal(self, signum, frame):
+        sys.exit(0)
+
+    def _clear_signal_handlers(self):
+        signal.signal(signal.SIGINT, self._handle_signal)
+        signal.signal(signal.SIGTERM, self._handle_signal)
+        signal.signal(signal.SIGHUP, signal.SIG_DFL)
+        signal.signal(signal.SIGUSR2, signal.SIG_DFL)
 
     @property
     def result(self):
-        self._result = self._queue.get()
+        if not self._result:
+            self._result = self._queue.get()
         return self._result
