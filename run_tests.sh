1f3ed77da6ffa236ddce367c8c9e44f09a2cbf64
Cenk Alti <cenkalti@gmail.com>
2023-12-06 14:42:39 +0300

    remove test container after run

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -euxo pipefail
 docker build -t kuyruk-test .
-exec docker run -it -v "/var/run/docker.sock:/var/run/docker.sock" kuyruk-test
+exec docker run -it --rm -v "/var/run/docker.sock:/var/run/docker.sock" kuyruk-test
